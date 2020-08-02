79ebf791b2dfeffe32ab98e8130bbd0e47b568fb
tatecarson <tatecarson@gmail.com>
2020-08-01 21:31:03 -0500

    spelling error

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ var $downloadSpeed = document.querySelector('#downloadSpeed')
 //   }
 // })
 
-client.add('https://swarm-test.netlify.app/nime2019_papper001.pdf.torrent', function (torrent) {
+client.add('https://swarm-test.netlify.app/nime2019_paper001.pdf.torrent', function (torrent) {
         // Got torrent metadata!
         console.log('Client is downloading:', torrent.infoHash)
         torrent.files[0].getBlobURL(function(err, url) {
